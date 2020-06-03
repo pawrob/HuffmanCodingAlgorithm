af2e3a52b2d579ae20838cb2519b246994a42cc4
Paweł Bucki <mrpawrob@gmail.com>
2020-06-03 17:23:03 +0200

    Dodanie komentarzy

--- client.py
+++ client.py
@@ -1,27 +1,28 @@
 import socket
 from Node import Node
+#ustawienenia naszego gniazda, port oraz nagłówek domyslny
 HEADER = 64
 PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "10.9.25.109"
+FORMAT = 'utf-8' #formatowanie tekstu
+DISCONNECT_MESSAGE = "!DISCONNECT" #formatowanie tekstu
+SERVER = "10.9.25.109" # ip klienta
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #tworzenie gniazda
+client.connect(ADDR)  #przypisanie adresu
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+def send(msg): #funkcja osblugujaca wysylanie informacji gniazdem
+    message = msg.encode(FORMAT) #deklaracja formatowania tekstu
+    msg_length = len(message) #okreslenei dlugosci tekstu
+    send_length = str(msg_length).encode(FORMAT) #nadanie dlugosci
+    send_length += b' ' * (HEADER - len(send_length)) #dodanie naglowka
+    client.send(send_length) #funkcja wysylajaca pakiet z dlugoscia wiadomosci
+    client.send(message) #funkcja wysylajaca wiadomosc
+    print(client.recv(2048).decode(FORMAT)) #potiwerdznei wyslania
 
 nodes = []
 initialNodes = []
-def codeMessage(message):
+def codeMessage(message): #kodowanie drzewa
     # ---------SORTOWANKO------------
     dictionary = {}
     for char in message:
@@ -46,24 +47,25 @@ string = 'ZAWSZE I WSZEDZIE POLICJA JEBANA BEDZIE!'
 
 class NodeTree(object):
     def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
-    def children(self):
+        self.left = left #przejscie na najblizsza lewa galaz
+        self.right = right #przejscie na najblizsza prawa galaz
+    def children(self): #definicja "potomka"
         return (self.left, self.right)
-    def nodes(self):
+    def nodes(self): #definicja wezla
         return (self.left, self.right)
-    def __str__(self):
+    def __str__(self): #funkcja przechowujaca wartosc dla danego wezla
         return '%s_%s' % (self.left, self.right)
 
-def huffmanCodeTree(node, left=True, binString=''):
+def huffmanCodeTree(node, left=True, binString=''): #implementacja drzewa oraz kodowania huffmana
     if type(node) is str:
-        return {node: binString}
-    (l, r) = node.children()
-    d = dict()
-    d.update(huffmanCodeTree(l, True, binString + '0'))
-    d.update(huffmanCodeTree(r, False, binString + '1'))
+        return {node: binString} #zamiana na binarny string odpowiedni do przeslania gniazdem
+    (l, r) = node.children() #deklaracja drzewa
+    d = dict() #deklaracja slownika
+    d.update(huffmanCodeTree(l, True, binString + '0')) # przypisanie 0 przy przejsciu na lewo
+    d.update(huffmanCodeTree(r, False, binString + '1'))  # przypisanie 1 przy przejsciu na prawo
     return d
 
+#sprawdzanie czestotliwosci wystepowania danego znaku w wiadomosci do zakodownaia
 freq = {}
 for c in string:
     if c in freq:
@@ -71,38 +73,34 @@ for c in string:
     else:
         freq[c] = 1
 
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-nodes = freq
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True) #sortowanie wg czestotliwosci
+nodes = freq #przepisanie wartosci
 
-while len(nodes) > 1:
-    (key1, c1) = nodes[-1]
+while len(nodes) > 1: #petla dla całego drzewa, tworzenei slownika
+    (key1, c1) = nodes[-1] #rozprowadzanie danych po drzewie
     (key2, c2) = nodes[-2]
     nodes = nodes[:-2]
     node = NodeTree(key1, key2)
     nodes.append((node, c1 + c2))
     nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
 
-huffmanCode = huffmanCodeTree(nodes[0][0])
-print(' Znak - Kod Huffmana ')
+huffmanCode = huffmanCodeTree(nodes[0][0]) #przekazanie slownika i danych do zmiennych
+print(' Znak - Kod Huffmana ') #wyswietlanie
 
-print(huffmanCode)
+print(huffmanCode) #wysiwetlanie zakodownaj wiadomosci
 messageToSend = ''
 dictionaryToSend = '/'
-for char in string:
+for char in string: #dodanie po znaku naszego kodu
     messageToSend = messageToSend + huffmanCode[char]
 
 #Przepisanie slownika na string
 for char in huffmanCode :
     dictionaryToSend = dictionaryToSend + char + ':' + huffmanCode[char] + ','
 
-print(dictionaryToSend)
-print(messageToSend)
-#Trzeba value puscic przez gniazdo wraz z dictionary
-#a nastepnie je rozszyfrować po stronie serwera zapewne
+print(dictionaryToSend) #wyswietlanie slownika
+print(messageToSend) #wyswietlanie wiadomosci
 
-send(dictionaryToSend)
-send(messageToSend)
+send(dictionaryToSend) #wysylanie slownika
+send(messageToSend) #wysylanie wiadomosci
 
-
-
-send(DISCONNECT_MESSAGE)
+send(DISCONNECT_MESSAGE) #koniec polaczneia
--- server.py
+++ server.py
@@ -1,77 +1,77 @@
 import socket
 import threading
-
+#ustawienenia naszego gniazda, port oraz nagłówek domyslny
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+FORMAT = 'utf-8' #formatowanie tekstu
+DISCONNECT_MESSAGE = "!DISCONNECT"#formatowanie tekstu
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #tworzenie gniazda
+server.bind(ADDR) #przypisanie adresu
 
-dictionary = {}
+dictionary = {} #deklaracja slownika do odebrnia
 
-def handle_message_dict( message ) :
+def handle_message_dict( message ) : #funkcja odbierająca i interpretujaca slownik
     i = 1
     dict = {}
-    while i < len(message)- 1 :
+    while i < len(message)- 1 : #podzial slownika na bity, odczytanie znaków
         dict[ message[i] ] = ''
         j = i
         i = i+2
         while message[i] is not ',' :
-            dict[ message[j] ] = dict[ message[j] ] + message[i]
+            dict[ message[j] ] = dict[ message[j] ] + message[i] #przepisywanie odczytanych znakow do slownika
             i = i + 1
         i = i + 1
-    print('Otrzymany slownik: ' , dict)
+    print('Otrzymany slownik: ' , dict) #wyswietlenie zawartosci
     return dict
 
-def translate(message,dict) :
+def translate(message,dict) : #funkjca tlumaczaca odebrane dane wg slownika
     solution = ''
     str = ''
     for char in message :
         str = str + char
         for element in dict :
-            if dict[element] == str :
+            if dict[element] == str : #porownainie bitowej postaci znaku do znakow ze slownika
                 solution = solution + element
                 str = ''
-    print ( 'Przetlumaczone ' + solution )
+    print ( 'Przetlumaczone ' + solution ) #wyswietlanie zawartosci
 
 
-def handle_client(conn, addr):
+def handle_client(conn, addr): #funkcja obslugujaca polaczenie gniazda
     print(f"[NEW CONNECTION] {addr} connected.")
 
-    connected = True
-    while connected:
+    connected = True #deklaracja otwartego polaczenia
+    while connected: #nastawienei na odbior podczas pracy programu
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
+            if msg == DISCONNECT_MESSAGE: #zamkniecie polaczenia po otrzymaniu wiadomosci
                 connected = False
 
             #print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
+            conn.send("Msg received".encode(FORMAT)) #potwierdzenie odbioru wiadomosci
 
 
-            if(msg[0] == '/') :
+            if(msg[0] == '/') : #odbieranie slownika
                 dictionary = handle_message_dict(msg)
-            else:
+            else: #odbieranie wiadomosci i jej tlumaczenie
                 translate(msg,dictionary)
 
-    conn.close()
+    conn.close() #zamkniecie polaczenia
 
 
 def start():
-    server.listen()
+    server.listen() #start nasluchiwania
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=handle_client, args=(conn, addr)) #rozpoczecie watku polaczenia
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}") #wyswietlanie aktywnych polaczen
 
 
 print("[STARTING] server is starting...")
-start()
+start() #wywolanie funkcji staru porgramu odbierajacego dane
